b6983646da7858bf1cc0bf387747b5da57ef0990
tanish_raj <tanish.quitegrowth@gmail.com>
2017-05-20 22:55:36 +0530

    Script updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 var modal = document.getElementById("modal-wrap");
 var modalButtom = document.getElementById("modalButton");
 var closeButtom = document.getElementById("closeButton");
+var modalWrap = document.getElementById("modal-wrap");
 
-modalButton.addEventListener('click', function(){
-	modal.style.visibility = (modal.style.visibility == "visible") ? "hidden" : "visible";
-})
+/* =====================================
+ESC key event handler to close modal
+======================================*/
+function keyEvenHandler(evt){
+	var keyCode = evt.keyCode;
+	if(keyCode == 27){
+		modal.style.visibility = (modal.style.visibility == "hidden") ? "visible" : "hidden";
+	}
+}
 
-closeButton.addEventListener('click', function(){
+/* =====================================
+Event handler to open a modal
+======================================*/
+function openModal(){
 	modal.style.visibility = (modal.style.visibility == "hidden") ? "visible" : "hidden";
-})
+}
+
+/* =====================================
+Event handler to close a modal
+======================================*/
+function closeModal(){
+	modal.style.visibility = (modal.style.visibility == "hidden") ? "visible" : "hidden";
+}
+
+/* =====================================
+Even Listeners for all modal actions
+======================================*/
+modalButton.addEventListener('click', openModal, false);
+closeButton.addEventListener('click', closeModal, false);
+document.addEventListener('keydown', keyEvenHandler, false);
